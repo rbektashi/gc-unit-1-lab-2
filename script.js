16442a45fe03d110da80cb709a09d887cb2a888a
Rae Bektashi <raeannamichele@gmail.com>
2020-09-24 17:19:49 -0400

    corrected var name

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ if (rain >= 20) {
 
 var fertilizer = prompt("Did you use fertilizer");
 if (fertilizer === "yes") {
-  var fertlizerType = prompt("Did you use premium or regular fertilizer?");
-  if (fertlizerType === "premium") {
+  var fertilizerType = prompt("Did you use premium or regular fertilizer?");
+  if (fertilizerType === "premium") {
     bushels = bushels + bushels * 0.15;
-  } else if (fertlizerType === "regular") {
+  } else if (fertilizerType === "regular") {
     bushels = bushels + bushels * 0.1;
   }
 }
